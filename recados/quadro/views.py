38f126d749bc52ffba49ebdd64403ddcb0dbc5fb
choba <marcelo.alckmin@gmail.com>
2024-02-23 12:56:04 -0300

    view básica

--- recados/quadro/views.py
+++ recados/quadro/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+from .models import Recado
+def index(request):
+    quantidade_recados = Recado.objects.count()
+    template = loader.get_template('quadro/index.html')
+    context = {
+    'quantidade_recados': quantidade_recados,
+    }
+    return HttpResponse(template.render(context, request))
