90e750721621e70f8b106210017bfc705b96ac98
choba <marcelo.alckmin@gmail.com>
2024-02-23 03:42:22 -0300

    deu errado por um espaço AAAA

--- recados/quadro/admin.py
+++ recados/quadro/admin.py
@@ -6,3 +6,5 @@ from django.contrib import admin
 from .models import Recado, Seguidor
 admin.site.register(Recado)
 admin.site.register(Seguidor)
+
+
--- recados/quadro/models.py
+++ recados/quadro/models.py
@@ -9,7 +9,7 @@ class Recado(models.Model):
  autor = models.ForeignKey('auth.User', related_name='recados',
  on_delete=models.CASCADE)
 
- class Seguidor(models.Model):
+class Seguidor(models.Model):
     usuario_seguidor = models.ForeignKey('auth.User', related_name='+',
     on_delete=models.CASCADE)
     usuario_seguido = models.ForeignKey('auth.User', related_name='+',
