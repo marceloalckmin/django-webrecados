39dd906bef92f3646c7fab8d9fd1d1cbb474184e
choba <marcelo.alckmin@gmail.com>
2024-02-23 01:57:19 -0300

    first commit

---
+++ recados/quadro/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Recado(models.Model):
+ titulo = models.CharField(max_length=100, blank=False)
+ data = models.DateTimeField(auto_now_add=True)
+ texto = models.TextField
+ autor = models.ForeignKey('auth.User', related_name='recados',
+ on_delete=models.CASCADE)
+
+ class Seguidor(models.Model):
+    usuario_seguidor = models.ForeignKey('auth.User', related_name='+',
+    on_delete=models.CASCADE)
+    usuario_seguido = models.ForeignKey('auth.User', related_name='+',
+    on_delete=models.CASCADE)
+
